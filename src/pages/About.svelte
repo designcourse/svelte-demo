101ca2ae8f33bc94263fc0d260248fed18c9bb48
Gary Simon <contact@designcourse.com>
2019-08-18 16:19:09 -0400

    first

---
+++ src/pages/About.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let router = {};
+
+  // Those contains useful information about current route status
+  router.path; // /test
+  router.route; // Route Object
+  router.params; // /about/bill/123/kansas { who: 'bill', where: 'kansas' }
+</script>
+
+<h1>About me</h1>
+
+<p>This is my router path: {router.path}</p>
